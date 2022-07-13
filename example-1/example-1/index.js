bc83702945115377f0aef9a6afa12b7502c14205
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-07-13 15:24:48 +0100

    Using a build tool

---
+++ example-1/example-1/index.js
@@ -0,0 +1,15 @@
+// console.log doesn't print anything on the page
+// it is not meant to be visible to the user, but for you
+// to help in debugging and getting visibility in your JS code.
+//
+// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+
+console.log('Hello from the developer console!');
+
+console.log(new Date());
+console.log('hey!!!!!');
+
+const add = require('./add');
+const multiply = require('./multiply');
+
+console.log(multiply(add(2,2), 4))
---
+++ example-1/example-1/multiply.js
@@ -0,0 +1,4 @@
+const multiply = (x,y) => {
+  return x * y;
+}
+module.exports = multiply;
